9e747c1ae88e13d8fe03ffe0ee827e435482786a
Ubuntu <ubuntu@ip-172-31-21-61.us-west-2.compute.internal>
2017-05-12 09:35:34 +0000

    Added logic for keeping track of highest completed difficulty, as well as logic for checking if the score file is in the rigjt format (format is x|y, where x can be any positive number and y is in the inclusive range of 1-5, separated by a pipe).

--- guess.sh
+++ guess.sh
@@ -2,13 +2,27 @@
 
 clear
 rand=$RANDOM
-if [[ ! -f score ]]
-then
-	echo 0 > score
+
+#create score file if it doesn't exist, setting default contents
+if [[ ! -f score ]]; then
+	echo "0|1" > score
 fi
+
 best=$(<score)
 
+#checks if variable best is in the right format, i.e. x|y, where x can be any positive number and y is in the inclusive range of 1-5, resets value if not.
+if [[ ! "$best" =~ (^[0-9]+\|[1-5]$) ]]; then
+	echo -e "\e[31mThe score file has been altered. Your lowest number of guesses and highest completed difficulty have been reset.\e[0m"
+	echo "0|1" > score
+	best=$(<score)
+fi
+
+#lowest is set to the number before the pipe, highest is set to the number after.
+lowest=${best%|*}
+highest=${best##*|}
+
 function game {
+	echo -e "Your lowest number of guesses is \e[36m$lowest\e[0m, and your highest completed difficulty is \e[36m$highest\e[0m."
 	read -p "How difficult would you like the game to be, from level 1-5? (1 = 1-digit random number, 5 = 5-digit random number) " diff
 	secret=${rand:0:$diff}
 	count=1
@@ -19,7 +33,7 @@ function game {
 		secret=$secret${rand2:0:1}
 	fi
 
-	showRandom
+#	showRandom
 
 	echo
 	read -p "Guess a random $diff digit number! " guess
@@ -45,38 +59,47 @@ function game {
 		echo -e "\e[32mCorrect!\e[0m \e[34m$secret\e[0m was the right number! It took you \e[96m$count guesses\e[0m! Good job!"
 	fi
 
-	#logic for output about their lowest number of guesses
-	if [[ "$count" -eq "$best" ]] && [[ "$best" -eq "1" ]]
+	#logic for output about their lowest number of guesses and highest difficulty
+	if [[ "$count" -eq "$lowest" ]] && [[ "$lowest" -eq "1" ]]
 	then
 		echo -e "Your lowest number of guesses was already \e[36m1\e[0m. A winner is still you! Try a higher difficulty, or delete the score file."
 		echo
-	elif [[ "$count" -eq "$best" ]] && [[ "$best" -ne "1" ]]
+	elif [[ "$count" -eq "$lowest" ]] && [[ "$lowest" -ne "1" ]]
 	then
 		echo -e "Your current number of guesses was tied for your best at \e[36m$count guesses\e[0m. You were so close!"
 		echo
-	elif [[ "$count" -lt "$best" ]] || [[ "$best" -eq "0" ]]
+	elif [[ "$count" -lt "$lowest" ]] || [[ "$lowest" -eq "0" ]]
 	then
-		prevBest=$best
-		best=$count
-		echo $best > score
-		if [[ "$prevBest" -eq "0" ]] && [[ "$best" -ne "1" ]]
-		then
-			echo -e "You set a new record for the lowest number of guesses at \e[36m$best\e[0m. Play again and see if you can beat it!"
+		prevlowest=$lowest
+		lowest=$count
+
+		if [[ "$prevLowest" -eq "0" ]]; then
+			echo -e "You set a new record for the lowest number of guesses at \e[36m$lowest\e[0m!"
+		else
+			echo -e "You beat your previous record of \e[36m$prevLowest guesses\e[0m! Good job!"
+		fi
+
+		if [[ "$highest" -eq "5" ]] && [[ "$lowest" -ne "1" ]]; then
+			echo -e "Your highest completed difficulty was already \e[36m5\e[0m. Try to beat your lowest number of guesses on this difficulty."
 			echo
-		elif [[ "$prevBest" -eq "0" ]] && [[ "$best" -eq "1" ]]
-		then
-			echo -e "You set a new record for the lowest number of guesses at \e[36m$best\e[0m. Try a higher difficulty, or delete the score file."
+		elif [[ "$highest" -eq "5" ]] && [[ "$lowest" -eq "1" ]]; then
+			echo -e "Your highest completed difficulty was already \e[36m5\e[0m. You have technically beaten the game! Congratulations!"
 			echo
-		else 
-			echo -e "You beat your previous record of \e[36m$prevBest guesses\e[0m! Good job!"
+		elif [[ "$diff" -gt "$highest" ]]; then
+			prevHighest=$highest
+			highest=$diff
+			echo -e "You have beaten your previous highest difficulty of \e[36m$prevHighest\e[0m. Your new highest difficulty is \e[36m$highest\e[0m!"
 			echo
 		fi
-	elif [[ "$count" -gt "$best" ]] && [[ "$best" -eq "1" ]]
+		echo "$lowest|$diff" > score
+	elif [[ "$count" -gt "$lowest" ]] && [[ "$lowest" -eq "1" ]]
 	then
-		echo -e "You did not beat your record of \e[36m$best guess\e[0m! Better luck next time!"
+		echo
+		echo -e "You did not beat your record of \e[36m$lowest guess\e[0m! Better luck next time!"
 		echo
 	else
-		echo -e "You did not beat your record of \e[36m$best guesses\e[0m! Better luck next time!"
+		echo
+		echo -e "You did not beat your record of \e[36m$lowest guesses\e[0m! Better luck next time!"
 		echo
 	fi
 }
@@ -107,7 +130,7 @@ if [[ -z "$1" ]]; then
 	echo "You will be offered a choice of difficulty from 1-5. The higher the difficulty, the more digits will be in the random number, i.e. Difficuty 1 is a 1-digit number from 1 to 9, but difficulty 5 will be a 5 digit number from 10000 to 99999."	
 	echo -e "Your best score (lowest number of guesses), as well as your highest difficulty completed will be stored in a file named 'score'."
 	echo
-	echo -e "To play the game, type \e[32m./guess.sh play\e[0m. To delete your score file, type \e[33m./guess.sh reset\e[0m."
+	echo -e "To play the game, type \e[32m./guess.sh play\e[0m. To delete your score file, type \e[31m./guess.sh reset\e[0m."
 	echo
 else
 	if [[ "$1" == "reset" ]]; then
@@ -116,13 +139,13 @@ else
 		if [[ $REPLY =~ ^[Yy]$ ]]
 		then
 			rm score
-			echo "Score file has been deleted."
+			echo -e "\e[31mScore file has been deleted.\e[0m"
 			echo
 		else
-			echo "Your score file has not been deleted."
+			echo -e "\e[32mYour score file has not been deleted.\e[0m"
 			echo
 		fi
-	elif [[ "$1" == "play" ]]; then 
+	elif [[ "$1" == "play" ]]; then
 		game
 	fi
 fi
