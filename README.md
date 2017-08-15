2ed4c51043e8e07299c14e7be27332955b8914ce
Robert Lent <lentcoding@outlook.com>
2017-08-15 03:31:26 -0400

    Update logic and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# bash-NumGuess
+# Bash Number Guess
+
+Bash Number Guess is a Bash number guessing game.
+
+The application asks the user for the numerical month and day of their birth, and will not allow them to enter any invalid entries for either. The user is limited to entering 1 through 12 for the month and 1 through the last day of their birth month for the day.
+
+### Requirements
+The application requires Python 3 be installed on your computer. After downloading and unzipping the files, you can then run the command 'python birthday.py' from inside of the unzipped folder.
--- guess.sh
+++ guess.sh
@@ -23,6 +23,7 @@ highest=${best##*|}
 
 function game {
 	echo -e "Your lowest number of guesses is \e[36m$lowest\e[0m, and your highest completed difficulty is \e[36m$highest\e[0m."
+	echo
 	read -p "How difficult would you like the game to be, from level 1-5? (1 = 1-digit random number, 5 = 5-digit random number) " diff
 	secret=${rand:0:$diff}
 	count=1
@@ -42,21 +43,23 @@ function game {
 		((count++))
 		if (($guess < $secret))
 		then
-			echo -n -e "\e[34m$guess\e[0m is \e[31mtoo low!\e[0m Try again! "
+			echo -n -e "\e[36m$guess\e[0m is \e[31mtoo low!\e[0m Try again! "
 			read guess
 		else
-			echo -n -e "\e[34m$guess\e[0m is \e[31mtoo high!\e[0m Try again! "
+			echo -n -e "\e[36m$guess\e[0m is \e[31mtoo high!\e[0m Try again! "
 			read guess
 		fi
 	done
 
+    clear
+
 	if [[ "$count" -eq "1" ]]
 	then
+		echo -e "\e[32mCorrect!\e[0m \e[36m$secret\e[0m was the right number! You got it in \e[96m$count guess\e[0m! How did you know?!"
 		echo
-		echo -e "\e[32mCorrect!\e[0m \e[34m$secret\e[0m was the right number! You got it in \e[96m$count guess\e[0m! How did you know?!"
 	else
+		echo -e "\e[32mCorrect!\e[0m \e[36m$secret\e[0m was the right number! It took you \e[96m$count guesses\e[0m! Good job!"
 		echo
-		echo -e "\e[32mCorrect!\e[0m \e[34m$secret\e[0m was the right number! It took you \e[96m$count guesses\e[0m! Good job!"
 	fi
 
 	#logic for output about their lowest number of guesses and highest difficulty
@@ -64,9 +67,11 @@ function game {
 	then
 		echo -e "Your lowest number of guesses was already \e[36m1\e[0m. A winner is still you! Try a higher difficulty, or delete the score file."
 		echo
+		echo
 	elif [[ "$count" -eq "$lowest" ]] && [[ "$lowest" -ne "1" ]]
 	then
 		echo -e "Your current number of guesses was tied for your best at \e[36m$count guesses\e[0m. You were so close!"
+		echo
 		echo
 	elif [[ "$count" -lt "$lowest" ]] || [[ "$lowest" -eq "0" ]]
 	then
@@ -82,24 +87,27 @@ function game {
 		if [[ "$highest" -eq "5" ]] && [[ "$lowest" -ne "1" ]]; then
 			echo -e "Your highest completed difficulty was already \e[36m5\e[0m. Try to beat your lowest number of guesses on this difficulty."
 			echo
+			echo
 		elif [[ "$highest" -eq "5" ]] && [[ "$lowest" -eq "1" ]]; then
 			echo -e "Your highest completed difficulty was already \e[36m5\e[0m. You have technically beaten the game! Congratulations!"
+			echo
 			echo
 		elif [[ "$diff" -gt "$highest" ]]; then
 			prevHighest=$highest
 			highest=$diff
 			echo -e "You have beaten your previous highest difficulty of \e[36m$prevHighest\e[0m. Your new highest difficulty is \e[36m$highest\e[0m!"
 			echo
+			echo
 		fi
 		echo "$lowest|$diff" > score
 	elif [[ "$count" -gt "$lowest" ]] && [[ "$lowest" -eq "1" ]]
 	then
-		echo
 		echo -e "You did not beat your record of \e[36m$lowest guess\e[0m! Better luck next time!"
 		echo
-	else
 		echo
+	else
 		echo -e "You did not beat your record of \e[36m$lowest guesses\e[0m! Better luck next time!"
+		echo
 		echo
 	fi
 }
@@ -125,12 +133,14 @@ function showRandom {
 }
 
 if [[ -z "$1" ]]; then
-	echo
 	echo -e "Welcome to my Bash Guessing Game!"
-	echo "You will be offered a choice of difficulty from 1-5. The higher the difficulty, the more digits will be in the random number, i.e. Difficuty 1 is a 1-digit number from 1 to 9, but difficulty 5 will be a 5 digit number from 10000 to 99999."	
-	echo -e "Your best score (lowest number of guesses), as well as your highest difficulty completed will be stored in a file named 'score'."
 	echo
-	echo -e "To play the game, type \e[32m./guess.sh play\e[0m. To delete your score file, type \e[31m./guess.sh reset\e[0m."
+	echo "You will be offered a choice of difficulty from 1-5. The higher the difficulty, the more digits will be in the number, i.e. Difficuty 1 is a 1-digit number from 1 to 9, but difficulty 5 will be a 5 digit number from 10000 to 99999."
+	echo
+	echo -e "Your best score (lowest number of guesses), as well as your highest completed difficulty will be stored in a file named 'score'."
+	echo
+	echo -e "To play the game, type \e[32m./guess.sh play\e[0m."
+	echo -e "To delete your score file, type \e[31m./guess.sh reset\e[0m."
 	echo
 else
 	if [[ "$1" == "reset" ]]; then
